9c93c2c82b34eef69fa99cb3c3cb7bb54d1b0058
Igor <reshetnikovia@outlook.com>
2021-03-31 14:00:04 +0400

    add methods to personalMovieDB

--- part_4/script.js
+++ part_4/script.js
@@ -1,30 +1,26 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов посмотрели?', '');
+        }
+    },
     rememberMyFilms: function () {
         for (let i = 0; i < 2; i++) {
             const a = prompt('Один из последних фильмов', ''),
                   b = prompt('На сколько оцените его', '');
         
             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
+                this.movies[a] = b;
                 console.log('done');
             } else {
                 console.log('error');
@@ -32,89 +28,42 @@ const personalMovieDB = {
             }
         }
     },
-
-
-};// почему не отображается а функции вызывать вручную
-
-
-
-function writeYourGenres() {
+    writeYourGenres: function () {
         for(let i = 1; i <= 3; i++){
             const genre = prompt(`Ваш любимый жанр под номером ${i}`);
             if (genre != null && genre != '') {
-                personalMovieDB.genres[i - 1] = genre;
+                this.genres[i - 1] = genre;
             } else {
                 i--;
             }
-            personalMovieDB.genres.forEach(function(item, i, genre){
-                console.log(`Любимый жанр # ${i + 1}- это ${item}`);
-            }) 
      }
-} // почему 5 раз
-
-writeYourGenres();
-
-
-/* 
-function writeYourGenres() {
-        for(let i = 1; i <= 3; i++){
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            if (genre === '' || genre == null) {
-               console.log('неверное значение');
-            } else {
-                 personalMovieDB.genres[i - 1] = genre;
-            }
-            personalMovieDB.genres.forEach(function(item, i) => {
-                console.log(`Любимый жанр ${i + 1}- это ${item}`);
-            }); 
-     }
-}
-*/
-
-    
-    
-    function detectPersonalLevel() {
-        if (personalMovieDB.count < 10) {
+     this.genres.forEach(function(item, i, genre){
+        console.log(`Любимый жанр # ${i + 1}- это ${item}`);
+    }) 
+    },
+    detectPersonalLevel: function() {
+        if (this.count < 10) {
             console.log('Мало');
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        } else if (this.count >= 10 && this.count < 30) {
             console.log('зритель');
-        } else if (personalMovieDB.count >= 30) {
+        } else if (this.count >= 30) {
             console.log('вы киноман');
         } else {
             console.log('error');
         }
-    }
-
-    detectPersonalLevel();
-
-    function snowMyDB () {
-        if (personalMovieDB.privat == false) {
-            console.log(personalMovieDB);
+    },
+    snowMyDB: function () {
+        if (this.privat == false) {
+            console.log(this);
         } else {
             console.log('error');
         }
-    }
-
-    snowMyDB ();
-
-
-function toggleVisibleMyDB () {
-    if (personalMovieDB.privat == false) {
-        personalMovieDB.privat = true;
-    }else if (personalMovieDB.privat == true) {
-        personalMovieDB.privat = false;
-    }
-    }
-
-toggleVisibleMyDB ();
-
-
-/*
-function toggleVisibleMyDB () {
-    if (personalMovieDB.privat) {
-        personalMovieDB.privat = false;
-    } else {
-        personalMovieDB.privat = true;
-    }
-    }
-*/
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat == false) {
+            personalMovieDB.privat = true;
+        }else if (personalMovieDB.privat == true) {
+            personalMovieDB.privat = false;
+        }
+        }
+};
