8055af1ea7d524347a5ab8be49ffe82335d9043f
Igor <reshetnikovia@outlook.com>
2021-04-11 23:03:08 +0400

    fix index

--- part_6/js/script.js
+++ part_6/js/script.js
@@ -1,70 +1,124 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-const img = document.querySelectorAll('.promo__adv img'),
-      genre = document.querySelector('.promo__genre'),
-      background = document.querySelector('.promo__bg'),
-      interactive = document.querySelectorAll('.promo__interactive-item'),
-      interactivelist = document.querySelector('.promo__interactive-list'),
-      ol = document.createElement('ol'),
-      btn = document.querySelector('button'),
-      input = document.querySelector('.input.value'),
-      chek = document.querySelector('[type="checkbox" i]');
- 
-
+    const img = document.querySelectorAll('.promo__adv img'),
+        genre = document.querySelector('.promo__genre'),
+        background = document.querySelector('.promo__bg'),
+        interactive = document.querySelectorAll('.promo__interactive-item'),
+        interactivelist = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        chek = addForm.querySelector('[type="checkbox"]');
 
 
-img.forEach(item => {
-    item.remove();
-});
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
+        let newFilm = addInput.value;
+        const favorite = chek.checked;
 
-genre.textContent = 'ДРАММА';
+        if (newFilm) {
 
-background.style.backgroundImage = 'url("img/bg.jpg")';
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
 
-movieDB.movies.sort();
+            if (favorite) {
+                console.log('Добавляем любимый фильм');
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, interactivelist);
+        }
+        event.target.reset ();
 
-interactivelist.innerHTML = '';
-
-movieDB.movies.forEach((film, i) => {
-    interactivelist.innerHTML += `
-    <li class="promo__interactive-item">${i + 1} ${film}
-         <div class="delete"></div>
-    </li>`;
-} );
-
-
-btn.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    movieDB.movies.push(input);
-    console.log(movieDB.movies);
     });
 
 
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    
+    const makeChanges = () => {
+        genre.textContent = 'ДРАММА';
+
+        background.style.backgroundImage = 'url("img/bg.jpg")';
+    };
 
 
-chek.addEventListener('click', () => {
-    console.log("Добавляем любимый фильм");
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+
+    movieDB.movies.sort();
+
+    function createMovieList(films, parent) {
+        sortArr(films);
+
+        parent.innerHTML = '';
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+             <div class="delete"></div>
+        </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+           btn.addEventListener('click', () => {
+               btn.parentElement.remove();
+               movieDB.movies.splice(i, 1);
+               createMovieList(films, parent);
+           });
+        });
+    };
+
+
+    deleteAdv(img);
+    makeChanges();
+    createMovieList(movieDB.movies, interactivelist);
+
 });
 
-movieDB.movies.sort();
 
 
-for (let value of movieDB.movies) {
-    if(value.length > 21){
-        value = value.substring(0, 21) + "...";
-    }
-}
+
+
+
+// btn.addEventListener('click', (ev) => {
+//     ev.preventDefault();
+//     movieDB.movies.push(input);
+//     console.log(movieDB.movies);
+//     });
+
+
+// chek.addEventListener('click', () => {
+//     console.log("Добавляем любимый фильм");
+// });
+
+// movieDB.movies.sort();
+
+
+// for (let value of movieDB.movies) {
+//     if(value.length > 21){
+//         value = value.substring(0, 21) + "...";
+//     }
+// }
 
 // 1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
 // новый фильм добавляется в список. Страница не должна перезагружаться.
